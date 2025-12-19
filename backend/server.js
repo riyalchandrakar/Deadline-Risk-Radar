82ec706d1921de973f72b77ea5e5e0a7349d2673
riyalchandrakar <riyalchandrakar6@gmail.com>
2025-12-20 00:39:36 +0530

    feat: add mongodb connection utility

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";   
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
+connectDB();
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
