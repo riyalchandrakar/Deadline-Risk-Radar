b4fd799a4ea5a17ba7652f9806b5ca8779cbb881
riyalchandrakar <riyalchandrakar6@gmail.com>
2025-12-20 18:07:52 +0530

    feat: implement jwt based authentication controller

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const generateToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists)
+    return res.status(400).json({ message: "User already exists" });
+
+  const user = await User.create({ name, email, password });
+
+  res.status(201).json({
+    _id: user.id,
+    name: user.name,
+    email: user.email,
+    token: generateToken(user.id)
+  });
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user || !(await user.matchPassword(password))) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  res.json({
+    _id: user.id,
+    name: user.name,
+    email: user.email,
+    token: generateToken(user.id)
+  });
+};
