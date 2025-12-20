95e40913faaae31c1cf1f32d23ea50dba1098090
riyalchandrakar <riyalchandrakar6@gmail.com>
2025-12-20 19:01:05 +0530

    feat: add task routes with authentication

---
+++ backend/src/routes/taskRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import protect from "../middleware/authMiddleware.js";
+import {
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask
+} from "../controllers/taskController.js";
+
+const router = express.Router();
+
+router.use(protect);
+
+router.route("/")
+  .post(createTask)
+  .get(getTasks);
+
+router.route("/:id")
+  .put(updateTask)
+  .delete(deleteTask);
+
+export default router;
